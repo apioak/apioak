217f526ac8d87111a23f0d1509a6dd73cccbd29a
Janko <shuaijinchao@gmail.com>
2019-11-22 00:07:16 +0800

    change: add rocks deps.

---
+++ apioak/pdk.lua
@@ -0,0 +1,7 @@
+return {
+    log  = require("apioak.pdk.log"),
+    ctx  = require("apioak.pdk.ctx"),
+    json = require("apioak.pdk.json"),
+    etcd = require("apioak.pdk.etcd"),
+    conf = require("apioak.pdk.config"),
+}
---
+++ apioak/pdk/log.lua
@@ -0,0 +1,22 @@
+local ngx_log = ngx.log
+
+local _NGX_LOG_LEVELS = {
+    debug  = ngx.DEBUG,
+    info   = ngx.INFO,
+    notice = ngx.NOTICE,
+    warn   = ngx.WARN,
+    err    = ngx.ERR,
+    crit   = ngx.CRIT,
+    alert  = ngx.ALERT,
+    emerg  = ngx.EMERG,
+}
+
+local _M = {}
+
+for log_name, log_level in pairs(_NGX_LOG_LEVELS) do
+    _M[log_name] = function(...)
+        ngx_log(log_level, ...)
+    end
+end
+
+return _M
