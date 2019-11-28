4414f2a9b97109c673856e212e234dfe9087a4e0
Janko <shuaijinchao@gmail.com>
2019-11-28 16:34:47 +0800

    feature: add plugin loading module.

---
+++ apioak/plugin/limit-req.lua
@@ -0,0 +1,7 @@
+local _M = {}
+
+function _M.http_access(oak_ctx)
+
+end
+
+return _M
--- apioak/sys/router.lua
+++ apioak/sys/router.lua
@@ -7,41 +7,6 @@ local router
 
 local _M = {}
 
-local function load_routes(premature)
-    if premature then
-        return
-    end
-    local etcd_routes, err = pdk.etcd.get('routes')
-    if err then
-        pdk.log.error("[sys.router] load router failure, " .. err)
-        return
-    end
-    local route_body = etcd_routes.body
-    local route_nodes = {}
-    if not route_body.node or not route_body.node.nodes then
-        pdk.log.error("[sys.router] router not set")
-    else
-        route_nodes = route_body.node.nodes
-    end
-
-    local default_method = { "GET", "PUT", "POST", "DELETE", "PATCH" }
-    for _, route_node in ipairs(route_nodes) do
-        if route_node.value.method then
-            default_method = { route_node.value.method }
-        end
-        table.insert(r3_route_nodes, {
-            path = route_node.value.uri,
-            method = default_method,
-            handler = function(param, oak_ctx)
-                oak_ctx.matched.param = param
-                oak_ctx.matched.route = route_node.value
-            end
-        })
-    end
-    r3 = r3route:new(r3_route_nodes)
-    r3:compile()
-end
-
 function _M.init_worker()
     ngx_timer_at(0, function (premature)
         if premature then
