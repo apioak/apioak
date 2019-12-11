1e8772e4f55b8b93afe4e6caf8ea01d9c5911c73
Janko <shuaijinchao@gmail.com>
2019-12-11 22:46:04 +0800

    feature: supported pdk.request get body.

--- apioak/pdk/request.lua
+++ apioak/pdk/request.lua
@@ -1,53 +1,66 @@
-local cjson = require("cjson")
+local ngx  = ngx
+local type = type
+local find = string.find
+local json = require("cjson.safe")
+local multipart = require "multipart"
+
+local CONTENT_TYPE           = "Content-Type"
+
+local CONTENT_TYPE_POST      = "application/x-www-form-urlencoded"
+local CONTENT_TYPE_JSON      = "application/json"
+local CONTENT_TYPE_FORM_DATA = "multipart/form-data"
 
 local _M = {}
--- 获取http get/post 请求参数
-function _M.getArgs(name, default)
-    local request_method = ngx.var.request_method
-    local args = {}
-    -- 参数获取
-    if "POST" == request_method then
-        ngx.req.read_body()
-        local postArgs = ngx.req.get_post_args()
-        if postArgs then
-            for k, v in pairs(postArgs) do
-                args[k] = v
-            end
-        end
+
+local function _header(key)
+    local headers = ngx.req.get_headers()
+    if key then
+        return headers[key]
     end
-    if name ~= nil then
-        if default == nil then
-            default = ''
-        end
-        return args[name] or default
-    end
-    return args
+    return headers
 end
 
-function _M.input(name, default)
-    local args = {}
-    local request_method = ngx.var.request_method
-    if (request_method == 'GET') then
-        args = ngx.req.get_uri_args()
+function _M.body()
+    local req_method = ngx.var.request_method
+    local content_type = _header(CONTENT_TYPE)
+    if req_method ~= "POST" then
+        return nil, "[pdk.request] " .. req_method .. " request cannot get body"
+    end
+
+    ngx.req.read_body()
+    if find(content_type, CONTENT_TYPE_POST, 1, true) == 1 then
+        local body_data, err = ngx.req.get_post_args()
+        if not body_data then
+            return nil, err
+        end
+
+        return body_data, nil
+
+    elseif find(content_type, CONTENT_TYPE_JSON, 1, true) == 1 then
+        local body, err = ngx.req.get_body_data()
+        if not body then
+            return nil, err
+        end
+
+        local body_data = json.decode(body)
+        if type(body_data) ~= "table" then
+            return nil, "[pdk.request] invalid json body"
+        end
+
+        return body_data, nil
+
+    elseif find(content_type, CONTENT_TYPE_FORM_DATA, 1, true) == 1 then
+        local body, err = ngx.req.get_body_data()
+        if not body then
+            return nil, err
+        end
+
+        return multipart(body, content_type):get_all(), nil
     else
-        ngx.req.read_body()
-        --获取body体
-        local body = ngx.req.get_body_data()
-        if (body ~= nil and body ~= '[]') then
-            local res, table_body = pcall(cjson.decode, body)
-            if res == false then
-                return args
-            end
-            args = table_body
-        end
+        return nil, "[pdk.request] unsupported content type '" .. content_type .. "'"
     end
-    if name ~= nil then
-        if default == nil then
-            default = ''
-        end
-        return args[name] or default
-    end
-    return args
 end
+
+_M.header = _header
 
 return _M
