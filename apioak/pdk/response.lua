bf023138430fca09d4ffd5e79cc7c36578f70f7f
Janko <shuaijinchao@gmail.com>
2019-12-11 23:31:22 +0800

    change: pdk.response exit enhancement.

--- apioak/pdk/response.lua
+++ apioak/pdk/response.lua
@@ -1,21 +1,43 @@
 local ngx = ngx
-local ngx_say    = ngx.say
-local ngx_exit   = ngx.exit
-local ngx_header = ngx.header
+local type = type
+local ngx_say     = ngx.say
+local ngx_exit    = ngx.exit
+local ngx_header  = ngx.header
+local json_encode = require("cjson.safe").encode
+
+local CONTENT_TYPE      = "Content-Type"
+
+local CONTENT_TYPE_JSON = "application/json"
+local CONTENT_TYPE_HTML = "text/html"
 
 local _M = {}
 
-function _M.exit(response)
-    local code = response.code or 404
-    local body = response.body or nil
-    local headers = response.headers or {}
-    for head_key, head_val in ipairs(headers) do
-        _M.set_header(head_key, head_val)
+function _M.exit(code, body)
+    if code and type(code) == "number" then
+        ngx.status = code
+    else
+        code = nil
     end
+
     if body then
-        ngx_say(body)
+        if type(body) == "table" then
+            local json_body, err = json_encode(body)
+            if err then
+                ngx_header[CONTENT_TYPE] = CONTENT_TYPE_HTML
+                ngx_say(err)
+            else
+                ngx_header[CONTENT_TYPE] = CONTENT_TYPE_JSON
+                ngx_say(json_body)
+            end
+        else
+            ngx_header[CONTENT_TYPE] = CONTENT_TYPE_HTML
+            ngx_say(body)
+        end
     end
-    ngx_exit(code)
+
+    if code then
+        ngx_exit(code)
+    end
 end
 
 function _M.set_header(key, value)
