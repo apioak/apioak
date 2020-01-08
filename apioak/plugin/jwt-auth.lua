27fbefe961e5a9dab08abf5c989b0e29fccc3d69
blust <wuxc67@sina.com>
2020-01-08 23:20:22 +0800

    update: optimization plugin-jwt-auth

--- apioak/plugin/jwt-auth.lua
+++ apioak/plugin/jwt-auth.lua
@@ -1,5 +1,7 @@
 local jwt      = require("resty.jwt")
 local pdk      = require("apioak.pdk")
+local plstring = require("pl.stringx")
+local ipairs   = ipairs
 
 local _M = {
     type  = 'Authentication',
@@ -18,27 +20,26 @@ local schema = {
     },
     required = { "secret" }
 }
-
-local function is_credential_in_header(secret, header_credential)
-    local res = jwt.verify(_M.key, secret, header_credential)
-    return res.verified
-end
-
-local function is_credential_in_query(secret, query_credential)
-    local res = jwt.verify(_M.key, secret, query_credential)
-    return res.verified
+local function jwt_auth(secret, credential)
+    local obj = jwt.verify(_M.key, secret, credential)
+    if obj.verified then
+        return true
+    end
+    return false
 end
 
 local function is_authorized(secret, header_credential, query_credential)
     if not secret then return false end
-    local authorized = false
-
-    if is_credential_in_header(secret, header_credential) then
-        authorized = true
-    elseif is_credential_in_query(secret, query_credential) then
-        authorized = true
+    local is_auth = false
+    if (not header_credential) and (not query_credential) then
+        return is_auth
     end
-    return authorized
+    if header_credential then
+        is_auth = jwt_auth(secret, plstring.split(header_credential, " ")[2])
+    elseif query_credential then
+        is_auth = jwt_auth(secret, query_credential)
+    end
+    return is_auth
 end
 
 function _M.http_access(oak_ctx)
@@ -60,7 +61,6 @@ function _M.http_access(oak_ctx)
     local query_credential = pdk.request.query('token')
 
     local is_success = is_authorized(plugin_conf.secret, header_credential, query_credential)
-
     if not is_success then
         pdk.response.exit(403, { err_message = "Authorization Required" })
     end
