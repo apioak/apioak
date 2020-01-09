d0dfc95549e4875843a1f13fc237d90fe7bf77df
zhanghy <291384433@qq.com>
2020-01-09 23:52:13 +0800

    change: modify the key verification in the key auth plugin.

--- apioak/plugin/key-auth.lua
+++ apioak/plugin/key-auth.lua
@@ -1,6 +1,4 @@
 local pdk      = require("apioak.pdk")
-local ipairs   = ipairs
-local tostring = tostring
 
 local _M = {
     type  = 'Authentication',
@@ -10,16 +8,21 @@ local _M = {
     order = 1201
 }
 
---check key exist
-local function key_exist(keys, key)
-    if not keys then
+local schema = {
+    type = "object",
+    properties = {
+        secret = {type = "string"},
+    },
+    required = { "secret" }
+}
+
+local function key_verify(header_key, secret)
+    if (not header_key) or (not secret) then
         return false
     end
 
-    for _, key_value in ipairs(keys) do
-        if (tostring(key) == tostring(key_value)) then
-            return true
-        end
+    if header_key == secret then
+        return true
     end
 
     return false
@@ -34,26 +37,27 @@ function _M.http_access(oak_ctx)
     if not oak_ctx.plugins[_M.key] then
         return false, nil
     end
-    --get key
-    local key = pdk.request.header('apikey')
-    if not key then
+
+    local plugin_conf = oak_ctx.plugins[_M.key]
+
+    ngx.say(pdk.json.encode(plugin_conf));
+
+    local _, err = pdk.schema.check(schema, plugin_conf)
+    if err then
+        return false, nil
+    end
+
+    local header_key = pdk.request.header('apikey')
+    if not header_key then
         pdk.response.exit(401, { err_message = "Missing API key found in request" })
     end
 
-    --get etcd keys
-    local etcd_cli = pdk.etcd.new()
-    local keys_etcd = etcd_cli.get(_M.name)
-    local keys_body = keys_etcd.body
-    if (not keys_body.node) or (not keys_body.node.value) then
-        pdk.response.exit(401, { err_message = "API key no set" })
-    end
-    local keys = pdk.json.decode(keys_body.node.value)
-
-    --check key exist
-    local exist = key_exist(keys, key)
-    if not exist then
+    local verify = key_verify(header_key, plugin_conf.secret)
+    if not verify then
         pdk.response.exit(401, { err_message = "Invalid API key in request" })
     end
+
+    return verify
 end
 
 return _M
