82586a62d1887538e156b19fa83bb5ebcd798033
zhanghy <291384433@qq.com>
2020-01-10 11:18:38 +0800

    change: edit apikey to

--- apioak/plugin/key-auth.lua
+++ apioak/plugin/key-auth.lua
@@ -16,12 +16,12 @@ local schema = {
     required = { "secret" }
 }
 
-local function key_verify(header_key, secret)
-    if (not header_key) or (not secret) then
+local function key_verify(secret_key, secret)
+    if (not secret_key) or (not secret) then
         return false
     end
 
-    if header_key == secret then
+    if secret_key == secret then
         return true
     end
 
@@ -40,21 +40,19 @@ function _M.http_access(oak_ctx)
 
     local plugin_conf = oak_ctx.plugins[_M.key]
 
-    ngx.say(pdk.json.encode(plugin_conf));
-
     local _, err = pdk.schema.check(schema, plugin_conf)
     if err then
         return false, nil
     end
 
-    local header_key = pdk.request.header('apikey')
-    if not header_key then
-        pdk.response.exit(401, { err_message = "Missing API key found in request" })
+    local secret_key = pdk.request.header('Authentication')
+    if not secret_key then
+        pdk.response.exit(401, { err_message = "Missing Authentication found in request" })
     end
 
-    local verify = key_verify(header_key, plugin_conf.secret)
+    local verify = key_verify(secret_key, plugin_conf.secret)
     if not verify then
-        pdk.response.exit(401, { err_message = "Invalid API key in request" })
+        pdk.response.exit(401, { err_message = "Invalid Authentication in request" })
     end
 
     return verify
