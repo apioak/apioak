0e06199474a71af255c063419c54c38d7f567fda
zhanghy <291384433@qq.com>
2020-01-10 12:56:28 +0800

    change: 1:delete schema validation for key-auth plugin parameters. 2:modify apikey to authentication. 3:modify header_key to secret_key. 4:adjust the overall structure.

--- apioak/plugin/key-auth.lua
+++ apioak/plugin/key-auth.lua
@@ -8,54 +8,32 @@ local _M = {
     order = 1201
 }
 
-local schema = {
-    type = "object",
-    properties = {
-        secret = {type = "string"},
-    },
-    required = { "secret" }
-}
-
 local function key_verify(secret_key, secret)
-    if (not secret_key) or (not secret) then
+    if secret_key ~= secret then
         return false
     end
 
-    if secret_key == secret then
-        return true
-    end
-
-    return false
+    return true
 end
 
-
 function _M.http_access(oak_ctx)
-    if not oak_ctx['plugins'] then
-        return false, nil
+
+    if oak_ctx['plugins'] and oak_ctx.plugins[_M.key] then
+
+        local plugin_conf = oak_ctx.plugins[_M.key]
+
+        if plugin_conf.secret then
+            local secret_key = pdk.request.header('Authentication')
+            if not secret_key then
+                pdk.response.exit(401, { err_message = "Missing Authentication found in request" })
+            end
+
+            local verify = key_verify(secret_key, plugin_conf.secret)
+            if not verify then
+                pdk.response.exit(401, { err_message = "Invalid Authentication in request" })
+            end
+        end
     end
-
-    if not oak_ctx.plugins[_M.key] then
-        return false, nil
-    end
-
-    local plugin_conf = oak_ctx.plugins[_M.key]
-
-    local _, err = pdk.schema.check(schema, plugin_conf)
-    if err then
-        return false, nil
-    end
-
-    local secret_key = pdk.request.header('Authentication')
-    if not secret_key then
-        pdk.response.exit(401, { err_message = "Missing Authentication found in request" })
-    end
-
-    local verify = key_verify(secret_key, plugin_conf.secret)
-    if not verify then
-        pdk.response.exit(401, { err_message = "Invalid Authentication in request" })
-    end
-
-    return verify
 end
 
 return _M
