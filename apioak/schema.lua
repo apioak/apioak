1c372d7aca657336f04ccbd1aaed9827dcd1c9cc
Janko <shuaijinchao@gmail.com>
2020-03-03 18:39:53 +0800

    feature: add apioak.schema control module.

---
+++ apioak/schema.lua
@@ -0,0 +1,7 @@
+return {
+    user    = require("apioak.schema.user"),
+    group   = require("apioak.schema.group"),
+    router  = require("apioak.schema.router"),
+    plugin  = require("apioak.schema.plugin"),
+    project = require("apioak.schema.project"),
+}
