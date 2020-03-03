61feba88d68feba066f9cc6228ee4ea0b1cd6b5b
Janko <shuaijinchao@gmail.com>
2020-03-03 18:39:09 +0800

    feature: add apioak.db control module.

---
+++ apioak/db.lua
@@ -0,0 +1,10 @@
+return {
+    user     = require("apioak.db.user"),
+    role     = require("apioak.db.role"),
+    group    = require("apioak.db.group"),
+    token    = require("apioak.db.token"),
+    router   = require("apioak.db.router"),
+    plugin   = require("apioak.db.plugin"),
+    project  = require("apioak.db.project"),
+    upstream = require("apioak.db.upstream"),
+}
