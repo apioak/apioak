7c1440406a83415de5d8fc751bbc81d0c0ba5867
Janko <shuaijinchao@gmail.com>
2020-03-03 18:29:35 +0800

    change: apioak.admin module.

--- apioak/admin.lua
+++ apioak/admin.lua
@@ -1,5 +1,7 @@
 return {
+    user    = require("apioak.admin.user"),
+    group   = require("apioak.admin.group"),
     router  = require("apioak.admin.router"),
+    project = require("apioak.admin.project"),
     plugin  = require("apioak.admin.plugin"),
-    service = require("apioak.admin.service"),
 }
