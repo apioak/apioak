28869af50562b278677ade4fa3dce0e56e1f0438
Janko <shuaijinchao@gmail.com>
2020-03-03 17:34:54 +0800

    feature: pdk.string add md5 and null functions.

--- apioak/pdk/string.lua
+++ apioak/pdk/string.lua
@@ -2,16 +2,22 @@ local plstring = require("pl.stringx")
 
 local _M = {}
 
-_M.format  = string.format
+_M.format   = string.format
 
-_M.lower   = string.lower
+_M.lower    = string.lower
 
-_M.upper   = string.upper
+_M.upper    = string.upper
 
-_M.find    = string.find
+_M.find     = string.find
 
-_M.split   = plstring.split
+_M.split    = plstring.split
 
-_M.replace = plstring.replace
+_M.replace  = plstring.replace
+
+_M.md5      = ngx.md5
+
+_M.null     = ngx.null
+
+_M.tonumber = tonumber
 
 return _M
