dff14d4bcd6e8c9ea74f859d8e7f0ac8a08ad291
Janko <shuaijinchao@gmail.com>
2020-03-09 21:55:48 +0800

    change: updated db.project query functions.

--- apioak/db/project.lua
+++ apioak/db/project.lua
@@ -1,10 +1,22 @@
-local pdk  = require("apioak.pdk")
+local pdk      = require("apioak.pdk")
+local plugin   = require("apioak.db.plugin")
+local upstream = require("apioak.db.upstream")
 
 local table_name = "oak_projects"
 
 local _M = {}
 
 _M.table_name = table_name
+
+function _M.all()
+    local sql = pdk.string.format("SELECT * FROM %s", table_name)
+    local res, err = pdk.mysql.execute(sql)
+
+    if err then
+        return nil, err
+    end
+    return res, nil
+end
 
 function _M.query_by_gid(group_id)
     local sql = pdk.string.format("SELECT * FROM %s WHERE group_id = %s", table_name, group_id)
@@ -58,6 +70,43 @@ function _M.delete(project_id)
         return nil, err
     end
     return res, nil
+end
+
+function _M.query_env_all()
+    local sql      = pdk.string.format("SELECT id, path FROM %s", table_name)
+    local res, err = pdk.mysql.execute(sql)
+
+    if err then
+        return nil, err
+    end
+
+    local projects = res
+    for i = 1, #projects do
+        local project = projects[i]
+        res, err      = plugin.query_by_res(plugin.RESOURCES_TYPE_PROJECT, project.id)
+        if err then
+            return nil, err
+        end
+
+        local plugins = {}
+        for p = 1, #res do
+            plugins[res[p].name] = res[p]
+        end
+        projects[i].plugins = plugins
+
+        res, err = upstream.query_by_pid(project.id)
+        if err then
+            return nil, err
+        end
+
+        local upstreams = {}
+        for u = 1, #res do
+            upstreams[res[u].env] = res[u]
+        end
+        projects[i].upstreams = upstreams
+    end
+
+    return projects, nil
 end
 
 return _M
