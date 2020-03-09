75c44afa1265a31bde19b3e2eac2085b2fb4d6e7
Janko <shuaijinchao@gmail.com>
2020-03-09 22:28:16 +0800

    feature: update route rewrite rules.

--- apioak/apioak.lua
+++ apioak/apioak.lua
@@ -1,5 +1,6 @@
 local ngx    = ngx
 local ipairs = ipairs
+local pairs  = pairs
 local pdk    = require("apioak.pdk")
 local sys    = require("apioak.sys")
 
@@ -37,6 +38,7 @@ function APIOAK.init_worker()
 end
 
 function APIOAK.http_access()
+
     local ngx_ctx = ngx.ctx
     local oak_ctx = ngx_ctx.oak_ctx
     if not oak_ctx then
@@ -44,24 +46,46 @@ function APIOAK.http_access()
         ngx_ctx.oak_ctx = oak_ctx
     end
 
-    local env = pdk.request.header("APIOAK-ENV")
+    local env = pdk.request.header(pdk.const.REQUEST_API_ENV_KEY)
     if env then
-        env = pdk.string.lower(env)
+        env = pdk.string.upper(env)
     else
-        env = pdk.admin.ENV_MASTER
+        env = pdk.const.ENVIRONMENT_PROD
     end
 
-    local routers = sys.router.get()
+    local routers  = sys.router.get()
     local match_ok = routers:dispatch("/" .. env .. ngx.var.uri, ngx.req.get_method(), oak_ctx)
     if not match_ok then
-        pdk.response.exit(404, { err_message = "\"URI\" not found" })
+        pdk.response.exit(404, { err_message = "\"URI\" Undefined" })
     end
 
-    if env == pdk.admin.ENV_MASTER then
-        pdk.response.exit(200, oak_ctx.response.success_content)
+    if oak_ctx.router.is_mock_request then
+        pdk.response.exit(200, oak_ctx.router.response_success)
     end
 
-    sys.router.analysis_uri(oak_ctx)
+    sys.router.init_request(oak_ctx)
+
+    local router  = oak_ctx.router
+    local matched = oak_ctx.matched
+
+    local upstream_uri = router.backend_path
+    for path_key, path_val in pairs(matched.path) do
+        upstream_uri = pdk.string.replace(upstream_uri, "{" .. path_key .. "}", path_val)
+    end
+
+    for header_key, header_val in pairs(matched.header) do
+        pdk.request.add_header(header_key, header_val)
+    end
+
+    local query_args = {}
+    for query_key, query_val in pairs(matched.query) do
+        pdk.table.insert(query_args, query_key .. "=" .. query_val)
+    end
+    if #query_args > 0 then
+        upstream_uri = upstream_uri .. "?" .. pdk.table.concat(query_args, "&")
+    end
+
+    ngx.var.upstream_uri = upstream_uri
 
     run_plugin("http_access", oak_ctx)
 end
