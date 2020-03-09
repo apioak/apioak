5fbef3fb725201a4843dc7af6684135f5d791e80
Janko <shuaijinchao@gmail.com>
2020-03-09 22:22:42 +0800

    change: update balancer module.

--- apioak/sys/balancer.lua
+++ apioak/sys/balancer.lua
@@ -1,91 +1,88 @@
-local pdk = require("apioak.pdk")
-local balancer = require("ngx.balancer")
+local pdk            = require("apioak.pdk")
+local balancer       = require("ngx.balancer")
 local balancer_chash = require('resty.chash')
 local balancer_round = require('resty.roundrobin')
 local set_current_peer = balancer.set_current_peer
 local get_last_failure = balancer.get_last_failure
-local set_timeouts = balancer.set_timeouts
-local set_more_tries = balancer.set_more_tries
-local ipairs = ipairs
-local tostring = tostring
-local tonumber = tonumber
+local set_more_tries   = balancer.set_more_tries
+local set_timeouts     = balancer.set_timeouts
 
 local _M = {}
 
 function _M.go(oak_ctx)
+    local router   = oak_ctx.router
+    local upstream = router.upstream
+    -- default enable tries
+    upstream.enable_tries = 1
 
-    local ngx_ctx = ngx.ctx
-    local upstream = oak_ctx.upstream or nil
-    if not upstream or not upstream.nodes then
-        pdk.log.error("[sys.balancer] upstream nodes not found")
+    if not upstream then
+        pdk.log.error("[sys.balancer] upstream undefined")
         pdk.response.exit(500)
     end
 
-    if not ngx_ctx.tries then
-        ngx_ctx.tries = pdk.const.TRY_DEFAULT_NUMBER
-    end
-    if not ngx_ctx.down then
-        ngx_ctx.down = {}
+    local try_nodes = upstream.try_nodes or pdk.table.new(10, 0)
+
+    local state, code = get_last_failure()
+    if state == "failed" then
+        pdk.log.error("[sys.balancer] connection " .. try_nodes[#try_nodes], " state: ", state, " code: ", code)
     end
 
-    local nodes = upstream.nodes
+    local nodes   = upstream.nodes
+    local servers = pdk.table.new(10, 0)
+    for i = 1, #nodes do
+        local node = pdk.string.format("%s:%s", nodes[i].ip, nodes[i].port)
+        if pdk.table.has(node, try_nodes) then
+            pdk.table.remove(nodes, i)
+        else
+            servers[node] = nodes[i].weight
+        end
+    end
+
+    if not servers or #nodes == 0 then
+        pdk.log.error("[sys.balancer] upstream.nodes undefined")
+        pdk.response.exit(500)
+    end
+
+    upstream.tries = #nodes - 1
+    if upstream.enable_tries == 0 or upstream.tries <= 1 then
+        upstream.tries = 0
+    end
+
+    set_more_tries(upstream.tries)
+
+    local timeout = upstream.timeout
+    if timeout then
+        local ok, err = set_timeouts(timeout.connect, timeout.send, timeout.read)
+        if not ok then
+            pdk.log.error("[sys.balancer] could not set upstream timeouts: ", err)
+        end
+    end
+
     local address
-    local try_number = tonumber(upstream.try_number) or pdk.const.TRY_MAX_NUMBER
-    local servers = {}
-    for _, server in ipairs(nodes) do
-        local key = server.ip .. ":" .. tostring(server.port)
-        if pdk.table.has(key, ngx_ctx.down) == false then
-            servers[key] = server.weight
-        end
+    if upstream.type == pdk.const.BALANCER_CHASH then
+        local chash_up = balancer_chash:new(servers)
+        address = chash_up:find(oak_ctx.matched.variable.remote_addr or pdk.const.LOCAL_IP)
     end
 
-    if #servers == 0 then
-        pdk.log.error("[sys.balancer] node all down")
+    if upstream.type == pdk.const.BALANCER_ROUNDROBIN then
+        local round_up = balancer_round:new(servers)
+        address = round_up:find()
+    end
+
+    if not address then
+        pdk.log.error("[sys.balancer] active upstream.nodes number is 0")
         pdk.response.exit(500)
     end
 
-    if ngx_ctx.tries == try_number then
-        local state, code = get_last_failure()
-        if ngx_ctx.host and ngx_ctx.port then
-            local down = ngx_ctx.host .. ":" .. tostring(ngx_ctx.port)
-            ngx_ctx.tries = pdk.const.TRY_DEFAULT_NUMBER
-            pdk.table.insert(ngx_ctx.down, down)
-        end
-        pdk.log.error("[sys.balancer] node state " .. state .. " code " .. code)
-    end
-
-    local ok, err = set_timeouts(pdk.const.BALANCER_COMMECT_TIMEOUT, pdk.const.BALANCER_SEND_TIMEOUT, pdk.const.BALANCER_READ_TIMEOUT)
+    address = pdk.string.split(address, ":")
+    local ok, err = set_current_peer(address[1], pdk.string.tonumber(address[2]))
     if not ok then
-        pdk.log.error("[sys.balancer] set timeouts error " .. err)
+        pdk.log.error("[sys.balancer] failed to set the current peer: ", err)
         pdk.response.exit(500)
     end
 
-    local ok, err = set_more_tries(try_number)
-    if not ok then
-        pdk.log.error("[sys.balancer] set more tries error " .. err)
-        pdk.response.exit(500)
-    end
-    ngx_ctx.tries = ngx_ctx.tries + pdk.const.TRY_INC_NUMBER
-
-    local upstream_type = upstream.type or pdk.const.BALANCER_ROUNDROBIN
-    if upstream_type == pdk.const.BALANCER_CHASH then
-        local hash = balancer_chash:new(servers)
-        address = hash:find(oak_ctx.request.client_ip or pdk.const.LOCAL_IP)
-    end
-
-    if upstream_type == pdk.const.BALANCER_ROUNDROBIN then
-        local round = balancer_round:new(servers)
-        address = round:find()
-    end
-
-    local server_info = pdk.string.split(address, ":")
-    ngx_ctx.host = server_info[1]
-    ngx_ctx.port = tonumber(server_info[2])
-    local ok, err = set_current_peer(ngx_ctx.host, ngx_ctx.port)
-    if not ok then
-        pdk.log.error("[sys.balancer] " .. address .. " error " .. err)
-        pdk.response.exit(500)
-    end
+    pdk.table.insert(try_nodes, address)
+    oak_ctx.router.upstream.try_nodes = try_nodes
 end
 
 return _M
