cd2f33e28d925cefe5d968a1022fae491dbb61dd
Janko <shuaijinchao@gmail.com>
2020-03-12 23:12:07 +0800

    change: remove apioak.db group module import.

--- apioak/db.lua
+++ apioak/db.lua
@@ -1,7 +1,6 @@
 return {
     user     = require("apioak.db.user"),
     role     = require("apioak.db.role"),
-    group    = require("apioak.db.group"),
     token    = require("apioak.db.token"),
     router   = require("apioak.db.router"),
     plugin   = require("apioak.db.plugin"),
