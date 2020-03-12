f9f35e257444c56d225bfbd76b1f7466d817b1fd
Janko <shuaijinchao@gmail.com>
2020-03-12 23:10:55 +0800

    change: remove apioak.admin group module import.

--- apioak/admin.lua
+++ apioak/admin.lua
@@ -1,6 +1,5 @@
 return {
     user    = require("apioak.admin.user"),
-    group   = require("apioak.admin.group"),
     router  = require("apioak.admin.router"),
     project = require("apioak.admin.project"),
     plugin  = require("apioak.admin.plugin"),
