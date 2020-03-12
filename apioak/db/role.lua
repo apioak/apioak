b7bac48bd8ea8ff70066bfce418b52a09c820755
Janko <shuaijinchao@gmail.com>
2020-03-12 23:01:54 +0800

    change: update db.role model functions.

--- apioak/db/role.lua
+++ apioak/db/role.lua
@@ -6,10 +6,11 @@ local _M = {}
 
 _M.table_name = table_name
 
-function _M.create(group_id, user_id, is_admin)
-    local sql = pdk.string.format("INSERT INTO %s (group_id, user_id, is_admin) VALUES ('%s', '%s', '%s')",
-            table_name, group_id, user_id, is_admin)
+function _M.create(project_id, user_id, is_admin)
+    local sql = pdk.string.format("INSERT INTO %s (project_id, user_id, is_admin) VALUES ('%s', '%s', '%s')",
+            table_name, project_id, user_id, is_admin)
     local res, err = pdk.mysql.execute(sql)
+
     if err then
         return nil, err
     end
@@ -17,10 +18,11 @@ function _M.create(group_id, user_id, is_admin)
     return res, nil
 end
 
-function _M.query(group_id, user_id)
-    local sql = pdk.string.format("SELECT * FROM %s WHERE group_id = %s AND user_id = %s",
-            table_name, group_id, user_id)
+function _M.query(project_id, user_id)
+    local sql = pdk.string.format("SELECT * FROM %s WHERE project_id = %s AND user_id = %s",
+            table_name, project_id, user_id)
     local res, err = pdk.mysql.execute(sql)
+
     if err then
         return nil, err
     end
@@ -28,10 +30,11 @@ function _M.query(group_id, user_id)
     return res, nil
 end
 
-function _M.delete(group_id, user_id)
-    local sql = pdk.string.format("DELETE FROM %s WHERE group_id = %s AND user_id = %s",
-            table_name, group_id, user_id)
+function _M.delete(project_id, user_id)
+    local sql = pdk.string.format("DELETE FROM %s WHERE project_id = %s AND user_id = %s",
+            table_name, project_id, user_id)
     local res, err = pdk.mysql.execute(sql)
+
     if err then
         return nil, err
     end
@@ -39,10 +42,11 @@ function _M.delete(group_id, user_id)
     return res, nil
 end
 
-function _M.update(group_id, user_id, is_admin)
-    local sql = pdk.string.format("UPDATE %s SET is_admin = %s WHERE group_id = %s AND user_id = %s",
-            table_name, is_admin, group_id, user_id)
+function _M.update(project_id, user_id, is_admin)
+    local sql = pdk.string.format("UPDATE %s SET is_admin = %s WHERE project_id = %s AND user_id = %s",
+            table_name, is_admin, project_id, user_id)
     local res, err = pdk.mysql.execute(sql)
+
     if err then
         return nil, err
     end
@@ -53,6 +57,7 @@ end
 function _M.query_by_uid(user_id)
     local sql = pdk.string.format("SELECT * FROM %s WHERE user_id = %s", table_name, user_id)
     local res, err = pdk.mysql.execute(sql)
+
     if err then
         return nil, err
     end
@@ -60,9 +65,10 @@ function _M.query_by_uid(user_id)
     return res, nil
 end
 
-function _M.query_by_gid(group_id)
-    local sql = pdk.string.format("SELECT * FROM %s WHERE group_id = %s", table_name, group_id)
+function _M.query_by_pid(project_id)
+    local sql = pdk.string.format("SELECT * FROM %s WHERE project_id = %s", table_name, project_id)
     local res, err = pdk.mysql.execute(sql)
+
     if err then
         return nil, err
     end
@@ -70,9 +76,10 @@ function _M.query_by_gid(group_id)
     return res, nil
 end
 
-function _M.delete_by_gid(group_id)
-    local sql = pdk.string.format("DELETE FROM %s WHERE group_id = %s", table_name, group_id)
+function _M.delete_by_pid(project_id)
+    local sql = pdk.string.format("DELETE FROM %s WHERE project_id = %s", table_name, project_id)
     local res, err = pdk.mysql.execute(sql)
+
     if err then
         return nil, err
     end
