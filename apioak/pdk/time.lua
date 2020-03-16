ebe6f916186f2f03108f93a0c47b69812f0fda84
Janko <shuaijinchao@gmail.com>
2020-03-16 22:52:19 +0800

    change: update pdk.time functions.

--- apioak/pdk/time.lua
+++ apioak/pdk/time.lua
@@ -4,12 +4,17 @@ _M.time      = os.time
 
 _M.date      = os.date
 
-_M.strtotime = function(date_str)
-    local _, _, year, month, day, hour, min, sec = string.find(date_str,
+_M.strtotime = function(date)
+    if not date or date == ngx.null then
+        return 0, "params \"date\" invalid"
+    end
+
+    local _, _, year, month, day, hour, min, sec = string.find(date,
             "(%d+)-(%d+)-(%d+)%s*(%d+):(%d+):(%d+)")
     if not year or not month or not day or not hour or not min or not sec then
-        return nil, "params \"date_str\" invalid"
+        return 0, "params \"date\" invalid"
     end
+
     return os.time({ year = year, month = month, day = day, hour = hour, min = min, sec = sec })
 end
 
