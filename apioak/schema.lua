8e1c2c377bd6898e56917aff0c05374e24025fcc
Janko <shuaijinchao@gmail.com>
2020-03-16 23:04:10 +0800

    change: apioak.schema add account and common import.

--- apioak/schema.lua
+++ apioak/schema.lua
@@ -1,6 +1,7 @@
 return {
     user    = require("apioak.schema.user"),
     router  = require("apioak.schema.router"),
-    plugin  = require("apioak.schema.plugin"),
     project = require("apioak.schema.project"),
+    account = require("apioak.schema.account"),
+    common  = require("apioak.schema.common"),
 }
