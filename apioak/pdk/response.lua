0712e96b8368f5aa8dc6c7a55419ec9008501a02
Janko <shuaijinchao@gmail.com>
2020-03-16 22:51:40 +0800

    change: update pdk.response functions.

--- apioak/pdk/response.lua
+++ apioak/pdk/response.lua
@@ -3,16 +3,12 @@ local type = type
 local ngx_say     = ngx.say
 local ngx_exit    = ngx.exit
 local ngx_header  = ngx.header
-local json_encode = require("cjson.safe").encode
-
-local CONTENT_TYPE      = "Content-Type"
-
-local CONTENT_TYPE_JSON = "application/json"
-local CONTENT_TYPE_HTML = "text/html"
+local const = require("apioak.pdk.const")
+local json  = require("apioak.pdk.json")
 
 local _M = {}
 
-function _M.exit(code, body)
+function _M.exit(code, body, content_type)
     if code and type(code) == "number" then
         ngx.status = code
     else
@@ -21,16 +17,16 @@ function _M.exit(code, body)
 
     if body then
         if type(body) == "table" then
-            local json_body, err = json_encode(body)
+            local res, err = json.encode(body)
             if err then
-                ngx_header[CONTENT_TYPE] = CONTENT_TYPE_HTML
+                ngx_header[const.CONTENT_TYPE] = const.CONTENT_TYPE_HTML
                 ngx_say(err)
             else
-                ngx_header[CONTENT_TYPE] = CONTENT_TYPE_JSON
-                ngx_say(json_body)
+                ngx_header[const.CONTENT_TYPE] = content_type or const.CONTENT_TYPE_JSON
+                ngx_say(res)
             end
         else
-            ngx_header[CONTENT_TYPE] = CONTENT_TYPE_HTML
+            ngx_header[const.CONTENT_TYPE] = content_type or const.CONTENT_TYPE_HTML
             ngx_say(body)
         end
     end
@@ -46,7 +42,7 @@ function _M.say(code, body)
     else
         code = nil
     end
-    ngx_header[CONTENT_TYPE] = CONTENT_TYPE_HTML
+    ngx_header[const.CONTENT_TYPE] = const.CONTENT_TYPE_HTML
     ngx_say(body)
 end
 
