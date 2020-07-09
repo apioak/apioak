905b75b33aee9d45c477cf7bd81654b99d4940ae
blust <wuxc67@sina.com>
2020-07-09 15:29:57 +0800

    change: modify Prometheus initialization location

--- apioak/plugin/prometheus.lua
+++ apioak/plugin/prometheus.lua
@@ -7,12 +7,33 @@ local _M = {
     name        = plugin_name,
     type        = "Service Monitoring",
     description = "Lua module for monitor server.",
+    init        = init
 }
 
 local config_schema = {
     type = "object",
     additionalProperties = false
 }
+
+-- 初始化 prometheus
+function _M.init()
+    prometheus = require("prometheus").init("prometheus_metrics")
+    -- http 请求数
+    metric.requests = prometheus:counter(
+            "nginx_http_requests_total", "Number of HTTP requests", {"host", "status"})
+    -- api 请求数
+    metric.api_requests = prometheus:counter(
+            "nginx_http_api_requests_total", "Number of HTTP requests", {"host", "api", "status"})
+    -- http 请求耗时
+    metric.latency = prometheus:histogram(
+            "nginx_http_request_duration_seconds", "HTTP request latency", {"host", "api"})
+    -- http 连接数
+    metric.connections = prometheus:gauge(
+            "nginx_http_connections", "Number of HTTP connections", {"state"})
+    -- 总带宽
+    metric.bandwidth = prometheus:counter("bandwidth", "Total bandwidth in bytes consumed per service in APIOAK",
+            {"host", "api"})
+end
 
 function _M.http_access(oak_ctx)
     local router  = oak_ctx.router or {}
--- apioak/sys/plugin.lua
+++ apioak/sys/plugin.lua
@@ -1,31 +1,28 @@
 local pdk = require("apioak.pdk")
+local config  = require("apioak.sys.config")
+local stringx = require("apioak.pdk.string")
 local _M = {}
 metric = {}
 
 function _M.init_worker()
-    -- 初始化 prometheus
-    init_prometheus()
 
-end
+    local res, err = config.query("plugins")
+    if err then
+        return
+    end
 
--- 初始化 prometheus
-function init_prometheus()
-    prometheus = require("prometheus").init("prometheus_metrics")
-    -- http 请求数
-    metric.requests = prometheus:counter(
-            "nginx_http_requests_total", "Number of HTTP requests", {"host", "status"})
-    -- api 请求数
-    metric.api_requests = prometheus:counter(
-            "nginx_http_api_requests_total", "Number of HTTP requests", {"host", "api", "status"})
-    -- http 请求耗时
-    metric.latency = prometheus:histogram(
-            "nginx_http_request_duration_seconds", "HTTP request latency", {"host", "api"})
-    -- http 连接数
-    metric.connections = prometheus:gauge(
-            "nginx_http_connections", "Number of HTTP connections", {"state"})
-    -- 总带宽
-    metric.bandwidth = prometheus:counter("bandwidth", "Total bandwidth in bytes consumed per service in APIOAK",
-            {"host", "api"})
+    for i = 1, #res do
+        local plugin_path = stringx.format("apioak.plugin.%s", res[i])
+        local ok, plugin = pcall(require, plugin_path)
+        if not ok then
+            pdk.log.error("failed to load plugin err: " .. plugin)
+            return
+        end
+        if plugin.init then
+            plugin.init()
+        end
+    end
+
 end
 
 return _M
