3000da384669685c067d033929cc98a90d240f75
zanehy <zanehy@foxmail.com>
2022-11-20 20:08:47 +0800

    feature: upgrade certificate admin-api

--- apioak/admin/certificate.lua
+++ apioak/admin/certificate.lua
@@ -6,6 +6,7 @@ local controller = require("apioak.admin.controller")
 local certificate_controller = controller.new("certificate")
 
 function certificate_controller.created()
+
     local body = certificate_controller.get_body()
 
     certificate_controller.check_schema(schema.certificate.created, body)
@@ -19,7 +20,8 @@ function certificate_controller.created()
     local exist_snis, exist_snis_err = dao.certificate.exist_sni(body.snis)
 
     if exist_snis_err ~= nil then
-        pdk.response.exit(500, { message = "sni detection failed [" .. exist_snis_err .. "]" })
+        pdk.log.error("certificate-create exception when checking if sni exists: [" .. exist_hosts_err .. "]")
+        pdk.response.exit(500, { message = "exception when checking if sni exists" })
     end
 
     if exist_snis and (#exist_snis > 0) then
@@ -30,7 +32,8 @@ function certificate_controller.created()
     local res, err = dao.certificate.created(body)
 
     if err then
-        pdk.response.exit(500, { message = err })
+        pdk.log.error("certificate-create create certificate exception: [" .. err .. "]")
+        pdk.response.exit(500, { message = "create certificate exception" })
     end
 
     pdk.response.exit(200, { id = res.id })
@@ -41,7 +44,8 @@ function certificate_controller.lists()
     local res, err = dao.certificate.lists()
 
     if err then
-        pdk.response.exit(500, { message = err })
+        pdk.log.error("certificate-list get certificate list exception: [" .. err .. "]")
+        pdk.response.exit(500, { message = "get certificate list exception" })
     end
 
     pdk.response.exit(200, res)
@@ -57,7 +61,8 @@ function certificate_controller.updated(params)
     local detail, err = dao.certificate.detail(body.certificate_key)
 
     if err then
-        pdk.response.exit(400, { message = err })
+        pdk.log.error("certificate-update get certificate detail exception: [" .. err .. "]")
+        pdk.response.exit(500, { message = "get certificate detail exception" })
     end
 
     if (body.name ~= nil) and (body.name ~= detail.name) then
@@ -72,7 +77,8 @@ function certificate_controller.updated(params)
     local exist_sni, exist_sni_err = dao.certificate.exist_sni(body.snis, detail.id)
 
     if exist_sni_err ~= nil then
-        pdk.response.exit(500, { message = "sni detection failed err:" .. exist_sni_err })
+        pdk.log.error("certificate-update exception when checking if sni exists: [" .. exist_sni_err .. "]")
+        pdk.response.exit(500, { message = "exception when checking if sni exists" })
     end
 
     if exist_sni and (#exist_sni > 0) then
@@ -83,7 +89,8 @@ function certificate_controller.updated(params)
     local res, err = dao.certificate.updated(body, detail)
 
     if err then
-        pdk.response.exit(500, { message = err })
+        pdk.log.error("certificate-update update certificate exception: [" .. err .. "]")
+        pdk.response.exit(500, { message = "update certificate exception" })
     end
 
     pdk.response.exit(200, { id = res.id })
@@ -96,7 +103,8 @@ function certificate_controller.detail(params)
     local detail, err = dao.certificate.detail(params.certificate_key)
 
     if err then
-        pdk.response.exit(400, { message = err })
+        pdk.log.error("certificate-detail get certificate detail exception: [" .. err .. "]")
+        pdk.response.exit(500, { message = "get certificate detail exception" })
     end
 
     pdk.response.exit(200, detail)
@@ -109,13 +117,19 @@ function certificate_controller.deleted(params)
     local detail, err = dao.certificate.detail(params.certificate_key)
 
     if err then
-        pdk.response.exit(400, { message = err })
+        pdk.log.error("certificate-delete get certificate detail exception: [" .. err .. "]")
+        pdk.response.exit(500, { message = "get certificate detail exception" })
+    end
+
+    if not detail then
+        pdk.response.exit(400, { message = "the certificate not found" })
     end
 
     local res, err = dao.certificate.deleted(detail)
 
     if err then
-        pdk.response.exit(500, { message = err })
+        pdk.log.error("certificate-delete remove certificate exception: [" .. err .. "]")
+        pdk.response.exit(500, { message = "remove certificate exception" })
     end
 
     pdk.response.exit(200, res)
--- apioak/admin/schema/certificate.lua
+++ apioak/admin/schema/certificate.lua
@@ -1,35 +1,32 @@
+local common = require "apioak.admin.schema.common"
+
 local _M = {}
+
+local cert_key = {
+    type      = "string",
+    minLength = 128,
+    maxLength = 64 * 1024
+}
+
+local snis = {
+    type        = "array",
+    uniqueItems = true,
+    minItems    = 1,
+    items       = {
+        type      = "string",
+        minLength = 3,
+        maxLength = 35,
+        pattern   = "^(?=^.{3,255}$)[a-zA-Z0-9-*-.][-a-zA-Z0-9]{0,62}(\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$",
+    }
+}
 
 _M.created = {
     type       = "object",
     properties = {
-        name = {
-            type      = "string",
-            minLength = 3,
-            maxLength = 35,
-            pattern   = "^\\*?[0-9a-zA-Z-_.]+$",
-        },
-        snis = {
-            type        = "array",
-            minItems    = 1,
-            uniqueItems = true,
-            items       = {
-                type      = "string",
-                minLength = 3,
-                maxLength = 35,
-                pattern   = "^(?=^.{3,255}$)[a-zA-Z0-9-*-.][-a-zA-Z0-9]{0,62}(\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$",
-            }
-        },
-        cert = {
-            type      = "string",
-            minLength = 128,
-            maxLength = 64 * 1024
-        },
-        key  = {
-            type      = "string",
-            minLength = 128,
-            maxLength = 64 * 1024
-        }
+        name = common.name,
+        snis = snis,
+        cert = cert_key,
+        key  = cert_key
     },
     required   = { "name", "snis", "cert", "key" }
 }
@@ -37,48 +34,11 @@ _M.created = {
 _M.updated = {
     type       = "object",
     properties = {
-        certificate_key = {
-            type  = "string",
-            anyOf = {
-                {
-                    minLength = 3,
-                    maxLength = 35,
-                    pattern   = "^\\*?[0-9a-zA-Z-_.]+$",
-                },
-                {
-                    minLength = 36,
-                    maxLength = 36,
-                    pattern   = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
-                }
-            }
-        },
-        name            = {
-            type      = "string",
-            minLength = 3,
-            maxLength = 35,
-            pattern   = "^\\*?[0-9a-zA-Z-_.]+$",
-        },
-        snis            = {
-            type        = "array",
-            uniqueItems = true,
-            minItems    = 1,
-            items       = {
-                type      = "string",
-                minLength = 3,
-                maxLength = 35,
-                pattern   = "^(?=^.{3,255}$)[a-zA-Z0-9-*-.][-a-zA-Z0-9]{0,62}(\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$",
-            }
-        },
-        cert = {
-            type      = "string",
-            minLength = 128,
-            maxLength = 64 * 1024
-        },
-        key  = {
-            type      = "string",
-            minLength = 128,
-            maxLength = 64 * 1024
-        }
+        certificate_key = common.param_key,
+        name            = common.name,
+        snis            = snis,
+        cert            = cert_key,
+        key             = cert_key
     },
     required   = { "certificate_key" }
 }
