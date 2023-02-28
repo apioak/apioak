2afd76320f27823def1231a1ccb1962c27c1cba0
zanehy <zanehy@foxmail.com>
2023-02-28 22:08:02 +0800

    feat: dependency installation localization

--- apioak/sys/meta.lua
+++ apioak/sys/meta.lua
@@ -1,7 +1,7 @@
 local version = setmetatable({
     major = 0,
-    minor = 5,
-    patch = 0,
+    minor = 6,
+    patch = 1,
 }, {
     __tostring = function(v)
         return string.format("%d.%d.%d", v.major, v.minor, v.patch)
